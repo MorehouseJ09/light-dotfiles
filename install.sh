f6ad24e5540d1c7b5dde7c20e49f1fe5fc0bb1bd
Jon Morehouse <morehousej09@gmail.com>
2013-08-09 02:53:16 -0700

    Update install

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 # initialize an install script to install the correct files
 for file in .bashrc .editrc .tmux.conf
 do
+	if [ -f $HOME/$file];
+	then
+		mv $HOME/$file $HOME/$file.orig		
+	fi
+
 	ln -s "$(pwd)/$file" $HOME/$file
 done
 

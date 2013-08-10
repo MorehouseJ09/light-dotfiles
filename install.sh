0e7843206047774abc5f5b14979dd75700f04674
Jon Morehouse <morehousej09@gmail.com>
2013-08-10 01:31:37 -0700

    Update install script from remote

--- install.sh
+++ install.sh
@@ -9,7 +9,20 @@ do
 	ln -s "$(pwd)/$file" $HOME/$file
 done
 
+# map up the vim directory
+if [ -f $HOME/.vim ]
+then
+	mv $HOME/.vim $HOME/.vim.orig
+fi
 
-# now run the vim install script
-$(pwd)/vim/install.sh
+# now link up the entire vim folder properly
+ln -s "$(pwd)/vim" $HOME/.vim
+
+# now link our vimrc proprly!
+# ensure the file doesn't exist and if it does move it
+if [ -f $HOME/.vimrc ]
+then
+	mv $HOME/.vimrc $HOME/.vimrc.orig
+fi
+ln -s "$(pwd)/vim/vimrc" $HOME/.vimrc
 

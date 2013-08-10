1d33b5fa0934117f86b5ab514a3389403ee6a412
Jon Morehouse <morehousej09@gmail.com>
2013-08-10 01:29:44 -0700

    Update application

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,9 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+# iniitalize other program shortcuts
+alias v='vim'
+
 # initialize tmux shortcuts etc
 alias switch='tmux switch -t ${1}'
 alias list='tmux list-sessions'
